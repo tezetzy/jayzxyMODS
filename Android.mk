699fec59c9bfe37f82373fb2bff42d3e215aa805
tezetzy <firlanrizqulloh@gmail.com>
2025-06-13 06:40:48 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_MODULE    := JayPatch
+LOCAL_MODULE    := jayzpatch
 LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
 LOCAL_C_INCLUDES += ./include
