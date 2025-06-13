d60681f1bfb6cad806b408dfa43979c461f98f04
tezetzy <firlanrizqulloh@gmail.com>
2025-06-13 09:19:09 +0700

    ASLI PART 6 ANJING

--- Android.mk
+++ Android.mk
@@ -6,4 +6,5 @@ LOCAL_MODULE    := jayzpatch
 LOCAL_SRC_FILES := bandul.cpp mod/logger.cpp mod/config.cpp
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
 LOCAL_C_INCLUDES += ./include
+LOCAL_LDLIBS := -llog
 include $(BUILD_SHARED_LIBRARY)
