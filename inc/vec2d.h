e0ad2f726cd8b761d6463af89bd9f83dead7cd77
tezetzy <firlanrizqulloh@gmail.com>
2025-06-13 07:25:59 +0700

    Update vec2d.h

--- inc/vec2d.h
+++ inc/vec2d.h
@@ -3,21 +3,21 @@
 
 #include <cmath>
 
-struct vec2d {
+struct Vec2D {
     double x, y;
 
     vec2d(double x = 0.0, double y = 0.0) : x(x), y(y) {}
 
-    vec2d operator+(const vec2d& v) const { return vec2d(x + v.x, y + v.y); }
-    vec2d operator-(const vec2d& v) const { return vec2d(x - v.x, y - v.y); }
-    vec2d operator*(double s) const { return vec2d(x * s, y * s); }
-    vec2d operator/(double s) const { return vec2d(x / s, y / s); }
+    vec2D operator+(const Vec2D& v) const { return Vec2D(x + v.x, y + v.y); }
+    vec2D operator-(const Vec2D& v) const { return Vec2D(x - v.x, y - v.y); }
+    vec2D operator*(double s) const { return Vec2D(x * s, y * s); }
+    vec2D operator/(double s) const { return Vec2D(x / s, y / s); }
 
-    double dot(const vec2d& v) const { return x * v.x + y * v.y; }
+    double dot(const Vec2D& v) const { return x * v.x + y * v.y; }
     double magnitude() const { return std::sqrt(x*x + y*y); }
-    vec2d normalize() const {
+    Vec2D normalize() const {
         double mag = magnitude();
-        return mag == 0 ? vec2d(0, 0) : vec2d(x / mag, y / mag);
+        return mag == 0 ? Vec2d(0, 0) : Vec2D(x / mag, y / mag);
     }
 };
 
