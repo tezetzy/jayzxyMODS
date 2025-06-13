1bac1352841db6b9f4dfc671e3e748b18d70998d
tezetzy <firlanrizqulloh@gmail.com>
2025-06-13 11:36:19 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_MODULE    := jayzpatch
-LOCAL_SRC_FILES := bandul.cpp mod/logger.cpp mod/config.cpp
+LOCAL_MODULE    := mirabyjay
+LOCAL_SRC_FILES := crossjay.cpp mod/logger.cpp
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
 LOCAL_C_INCLUDES += ./include
 LOCAL_LDLIBS := -llog
