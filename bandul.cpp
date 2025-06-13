cfc7c941d339ba0d1bc964d7d00b164b48d7ddc8
tezetzy <firlanrizqulloh@gmail.com>
2025-06-13 10:36:52 +0700

    kinggggg
    fix fc

--- bandul.cpp
+++ bandul.cpp
@@ -6,24 +6,16 @@
 
 MYMODCFG(net.jayzxy.settings, Jtings, 1.0, Jayzxy)
 
-/////////////////////////////////////////////////////////////////////////////
-///////////////////////////////     Saves     ///////////////////////////////
-/////////////////////////////////////////////////////////////////////////////
 uintptr_t pGTASA;
 void* hGTASA;
 static constexpr float fMagic = 50.0f / 30.0f;
 
-/////////////////////////////////////////////////////////////////////////////
-///////////////////////////////     Vars      ///////////////////////////////
-/////////////////////////////////////////////////////////////////////////////
 float *ms_fTimeStep, *m_f3rdPersonCHairMultX, *m_f3rdPersonCHairMultY;
-/////////////////////////////////////////////////////////////////////////////
-///////////////////////////////     Funcs     ///////////////////////////////
-/////////////////////////////////////////////////////////////////////////////
+
 struct CVector2D {
     float x, y;
 };
-////
+
 void Redirect(uintptr_t addr, uintptr_t to)
 {
     if(!addr) return;
@@ -85,7 +77,7 @@ extern "C" void OnModLoad()
     //Fix walking while rifle-aiming
     if(cfg->Bind("FixAimingWalkRifle", true, "Gameplay")->GetBool())
     {
-        HOOKPLT(ControlGunMove, pGTASA + 0x66F9D0);
+        HOOKPLT(HookOf_ControlGunMove, pGTASA + 0x66F9D0);
     }
 
     //Fix crosshair position
@@ -94,6 +86,6 @@ extern "C" void OnModLoad()
         aml->Unprot(pGTASA + 0x952CB0, 8);
         SET_TO(m_f3rdPersonCHairMultX, pGTASA + 0x952CB0);
         SET_TO(m_f3rdPersonCHairMultY, pGTASA + 0x952CB4);
-        HOOKPLT(DrawCrosshair, pGTASA + 0x672880);
+        HOOKPLT(HookOf_DrawCrosshair, pGTASA + 0x672880);
     }
 }
