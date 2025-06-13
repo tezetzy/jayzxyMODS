b20d77b898ca828e6849f6001c319e81a5008903
tezetzy <firlanrizqulloh@gmail.com>
2025-06-14 00:23:23 +0700

    Update libjaypatch.cpp

--- libjaypatch.cpp
+++ libjaypatch.cpp
@@ -1,6 +1,5 @@
 #include <mod/amlmod.h>
 //#include <mod/config.h>
-#include <struc/crosshair.h>
 
 MYMOD(net.jayzxy.gtasa.abtfixes, GTA:SA ABT Fixes, 1.0, Jayzxy)
 NEEDGAME(com.rockstargames.gtasa)
@@ -13,12 +12,13 @@ float* m_f3rdPersonCHairMultY;
 float* fAspectCorrection;  // biasanya aspek rasio (w/h)
 float* ar43;
 float* ms_fTimeStep;
-float save;
-static constexpr float ar43 = 4.0f / 3.0f;
-static constexpr float fMagic = 50.0f / 30.0f;
+float* save;
+
+constexpr float fMagic = 50.0f / 30.0f;
 
 /* vars */
 float fAspectCorrection = 0.0f, fAspectCorrectionDiv = 0.0f;
+constexpr float ar43 = 4.0f / 3.0f;
 #define fAspectCorrection (*ms_fAspectRatio - ar43)
 #define fAspectCorrectionDiv (*ms_fAspectRatio / ar43)
 #define GetTimeStep() (*ms_fTimeStep)
@@ -28,6 +28,33 @@ float fAspectCorrection = 0.0f, fAspectCorrectionDiv = 0.0f;
 
 /* hoam */
 CPlayerPed* (*FindPlayerPed)(int playerId);
+/* lele */
+// Struct weapon & player
+enum eWeaponType
+{
+    WEAPON_UNARMED = 0,
+    WEAPON_COUNTRYRIFLE = 33,
+    // Tambahkan jika perlu
+};
+
+struct CWeapon
+{
+    int m_eWeaponType;
+    // Tambahan field jika perlu
+};
+
+struct CPlayerPed
+{
+    char pad[0x5A0];                         // Perkirakan offset ke weapon
+    CWeapon m_WeaponSlots[13];               // 13 slot senjata
+    int m_nCurrentWeapon;
+};
+
+struct CPlayerInfo
+{
+    CPlayerPed* pPed;
+};
+CPlayerInfo* WorldPlayers;
 
 float fWideScreenWidthScale, fWideScreenHeightScale;
 DECL_HOOKv(DrawCrosshair)
@@ -65,12 +92,14 @@ extern "C" void OnModLoad()
     pGTASA = aml->GetLib("libGTASA.so");
     hGTASA = aml->GetLibHandle("libGTASA.so");
 
-    aml->Unprot(pGTASA + 0x952CB0, 8);
     SET_TO(ms_fTimeStep,            aml->GetSym(hGTASA, "_ZN6CTimer12ms_fTimeStepE"));
     SET_TO(m_f3rdPersonCHairMultX,  aml->GetSym(hGTASA, "_ZN7CCamera22m_f3rdPersonCHairMultXE"));
     SET_TO(m_f3rdPersonCHairMultY,  aml->GetSym(hGTASA, "_ZN7CCamera22m_f3rdPersonCHairMultYE"));
     SET_TO(ms_fAspectRatio,         aml->GetSym(hGTASA, "_ZN5CDraw15ms_fAspectRatioE"));
     SET_TO(ms_fTimeScale,           aml->GetSym(hGTASA, "_ZN6CTimer13ms_fTimeScaleE"));
+    SET_TO(WorldPlayers,            *(void**)(pGTASA + BYBIT(0x6783C8, 0x84E7A8)));
+    
+    
     HOOKPLT(DrawCrosshair, pGTASA + 0x51C694);
     HOOKPLT(ControlGunMove, pGTASA + 0x83F9D8);
 }
