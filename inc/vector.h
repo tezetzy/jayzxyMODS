7db0b6221d4757e2dacf9850e76d9c0b94032508
tezetzy <firlanrizqulloh@gmail.com>
2025-06-13 12:50:52 +0700

    Create vector.h

---
+++ inc/vector.h
@@ -0,0 +1,14 @@
+// Di vector.h
+#ifndef __MY_CVECTOR_H__
+#define __MY_CVECTOR_H__
+
+struct CVector { float x, y, z; };
+struct CVector2D { float x, y; };
+enum RwRenderState {
+    rwRENDERSTATEZTESTENABLE = 4,
+    rwRENDERSTATEZWRITEENABLE = 7,
+    rwRENDERSTATEVERTEXALPHAENABLE = 22
+    // dll
+};
+
+#endif
