f81773e5848a1e723aea23f6fd71a8155adaacc8
tezetzy <firlanrizqulloh@gmail.com>
2025-06-13 10:51:28 +0700

    hai

--- bandul.cpp
+++ bandul.cpp
@@ -10,12 +10,11 @@ uintptr_t pGTASA;
 void* hGTASA;
 static constexpr float fMagic = 50.0f / 30.0f;
 
-float *ms_fTimeStep, *m_f3rdPersonCHairMultX, *m_f3rdPersonCHairMultY;
+float *ms_fTimeStep;
 
 struct CVector2D {
     float x, y;
 };
-float* ms_fAspectRatio = nullptr
 
 void Redirect(uintptr_t addr, uintptr_t to)
 {
@@ -51,20 +50,6 @@ void HookOf_ControlGunMove(void* self, CVector2D* vec2D)
     ControlGunMove(self, vec2D);
     *ms_fTimeStep = save;
 }
-
-DECL_HOOKv(DrawCrosshair);
-//
-static constexpr float XSVal = 1024.0f / 1920.0f; // prev. 0.530, now it's 0.533333..3
-static constexpr float YSVal = 768.0f / 1920.0f; // unchanged :p
-void HookOf_DrawCrosshair()
-{
-    float save1 = *m_f3rdPersonCHairMultX; *m_f3rdPersonCHairMultX = 0.530f;
-    float save2 = *m_f3rdPersonCHairMultY; *m_f3rdPersonCHairMultY = 0.530f;
-    DrawCrosshair();
-
-    *m_f3rdPersonCHairMultX = save1;
-    *m_f3rdPersonCHairMultY = save2;
-}
 /////////////////////////////
 extern "C" void OnModLoad()
 {
@@ -87,9 +72,6 @@ extern "C" void OnModLoad()
     //Fix crosshair position
     if(cfg->Bind("FixCrosshair", true, "Visual")->GetBool())
     {
-        aml->Unprot(pGTASA + 0x952CB0, 8);
-        SET_TO(m_f3rdPersonCHairMultX, pGTASA + 0x952CB0);
-        SET_TO(m_f3rdPersonCHairMultY, pGTASA + 0x952CB4);
-        HOOKPLT(HookOf_DrawCrosshair, pGTASA + 0x672880);
+        //null
     }
 }
