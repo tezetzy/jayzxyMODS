5a8d5e2c5fc83886bd811e7c3ce83b5557fce4e2
tezetzy <firlanrizqulloh@gmail.com>
2025-06-13 21:37:51 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,9 +2,15 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_MODULE    := AbtFixes
-LOCAL_SRC_FILES := libjaypatch.cpp mod/logger.cpp mod/config.cpp
+ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+	LOCAL_MODULE := AbtFixes
+else
+	LOCAL_MODULE := AbtFixes64
+endif
+LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
-LOCAL_C_INCLUDES += ./include
-LOCAL_LDLIBS := -llog
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/patches
+ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+	LOCAL_CFLAGS += -mfpu=neon
+endif
 include $(BUILD_SHARED_LIBRARY)
